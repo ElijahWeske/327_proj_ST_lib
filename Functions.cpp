f11f8caba724194c76ffffbdea194ac7058962c7
Elijah Weske <54817873+ElijahWeske@users.noreply.github.com>
2019-11-22 12:32:25 -0500

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -4,56 +4,59 @@
  *  Last edited on: Nov 20, 2019
  *      Author: Elijah Weske
  *	
- *  Created on: Nov 5, 2017
- *  	Created by: Keith Perkins
+ *  Created by: Keith Perkins
  */
 
 #include "./includes/Functions.h"
+#include "./includes/Smalltalk.h"
 #include "./includes/Smalltalk_American.h"
 #include "./includes/ST_American_DonutEnthusiest.h"
 #include "./includes/Smalltalk_Brit.h"
 #include "./includes/Watch.h"
 #include "./includes/constants.h"
 
-//create a vector with a brit,american, donut enthusiest using unique pointers
-//use c++11 and then returning by value is fine since the compiler will move the vector
-//on return rather than recreate it(if using C++11).
+//create a vector with appropriate numbers of Smalltalk_Brit,Smalltalk_American and ST_American_DonutEnthusiest
+//objects using unique pointers. 
+//Watches are given sequentially starting with first person in the vector until 
+//all watches given or everyone has a watch (case where more watches than people)
+//Since we are using c++11 returning this vector by value is fine since the 
+//compiler will move the vector on return rather than recreate it (this means there is no copy penalty)
 std::vector<std::unique_ptr<Smalltalk>> getPeople(int numBrit, int numAmerican,
 		int numAmericanDonutEnthusiest, int numWatches) {
 			
-	//create a vector to hold SmallTalk unique pointers
+	//create a temp_vector to hold SmallTalk unique pointers
 	std::vector<std::unique_ptr<Smalltalk>> temp_vector;
 	
-	//add brits to vector
+	//add Brits to temp_vector
 	for (int i = 0; i < numBrit; i++) {
 		temp_vector.push_back(std::unique_ptr<Smalltalk_Brit>(new Smalltalk_Brit(i)));
 	}
 	
-	//add americans  to vector
+	//add Americans to temp_vector
 	for (int i = 0; i < numAmerican; i++) {
 		temp_vector.push_back(std::unique_ptr<Smalltalk_American>(new Smalltalk_American(i)));
 	}
 	
-	//add american donut "enthusiest"  to vector
+	//add American Donut Enthusiest to temp_vector
 	for (int i = 0; i < numbAmericanDonutEnthusiest; i++) {
 		temp.push_back(std::unique_ptr<ST_American_DonutEnthusiest>(new ST_American_DonutEnthusiest(i)));
 	}
 	
-	//create some watches (as long as num watches <= num people)	
-	//then give the watches away to first NUM_WATCHES people in the vector
-	//keep in mind that you are responsible for deleting any pointers to 
-	//watches allocated on the heap when you are finished using the vector you return 
-	//from this function(see Smalltalk header for hints)
+	//total number of people for easy comparison with number of watches
 	int numPeople = numBrit + numAmerican + numAmericanDonutEnthusiest;
 	
+	// if there are more watches than people, set watches to people.
 	if (numWatches > numPeople){
 		numWatches = numPeople;
 	}
 	
-	for (int i = 0; i < (int)temp_vector.size(); i++){
+	//Watches are given sequentially starting with first person in the vector until 
+	//all watches given or everyone has a watch (case where more watches than people)
+	//create and give watches to everyone
+	for (int i = 0; i < numPeople; i++){
 		temp_vector[i]->giveWatch(new Watch());
 	}
 	
-	//return your vector
+	//return the vector
 	return temp_vector;
 }
